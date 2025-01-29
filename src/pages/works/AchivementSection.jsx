ac818e1e884182a1563a48113bed7c23fb31d974
Afsal Ka <afsalafzz63@gmail.com>
2025-01-29 09:30:01 +0530

    works section updating

--- src/pages/works/AchivementSection.jsx
+++ src/pages/works/AchivementSection.jsx
@@ -4,24 +4,29 @@ function AchivementSection() {
   return (
     <div className=" mt-[85px] mb-[53px] w-full">
       <div>
-        <p className="text-[48px] text-center text-black poppins-semibold">
-          Crafting Bold Ideas and Innovative Designs to Build <br /> Unique
-          Stories, Captivating Experiences, and <br /> Transformative Brand
-          Journeys Globally
+        <p className="text-[48px] text-center text-black font-[boldtext]">
+          Crafting Bold{" "}
+          <span>
+            Ideas and Innovative Designs to Build <br /> Unique Stories,{" "}
+          </span>{" "}
+          Captivating Experiences, and <br />{" "}
+          <span>Transformative Brand Journeys Globally</span>
         </p>
       </div>
       <div className="mt-[112px] flex justify-center  gap-[120px]">
         <div className="flex flex-col   items-center ">
-            <p className="text-[64px] poppins-semibold ">50+</p>
-            <p className="text-[21px] text-center">Brands</p>
+          <p className="text-[64px] font-[boldtext] ">50+</p>
+          <p className="text-[21px] text-center">Brands</p>
         </div>
         <div className="flex flex-col   items-center ">
-            <p className="text-[64px] poppins-semibold">500+</p>
-            <p className="text-[21px] text-center">Projects Launched <br /> across India and GCC</p>
+          <p className="text-[64px] font-[boldtext]">500+</p>
+          <p className="text-[21px] text-center">
+            Projects Launched <br /> across India and GCC
+          </p>
         </div>
         <div className="flex flex-col   items-center">
-            <p className="text-[64px] poppins-semibold">20+</p>
-            <p className="text-[21px] text-center">Ad films</p>
+          <p className="text-[64px] font-[boldtext]">20+</p>
+          <p className="text-[21px] text-center">Ad films</p>
         </div>
       </div>
     </div>
--- src/pages/works/Banner.jsx
+++ src/pages/works/Banner.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Banner() {
   return (
     <div className="bg-black flex justify-start items-center px-[100px] min-h-[700px]">
-      <div className="w-10/12">
-        <p className="text-white poppins-semibold text-[80px]  leading-[90px]">
+      <div className="w-8/12">
+      <p className="text-white text-[40px] lg:text-[60px]  xl:text-[70px] lg:leading-[80px] font-[boldtext]">
           Creative and Communication
         </p>
       </div>
