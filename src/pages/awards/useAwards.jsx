c892c13ac736f14084454210e942e268f995b7eb
undefined-coder-1 <devs4webs@gmail.com>
2025-02-01 10:08:40 +0530

    rebased from rm-pages

--- src/pages/awards/useAwards.jsx
+++ src/pages/awards/useAwards.jsx
@@ -21,7 +21,7 @@ export default function useAwards() {
             date: "2023",
             desc: "At Haris&Co., we understand that your brand is more than just a logo or a tagline—it's the essence of your business, the story you tell, and the promise you make to your customers. Our branding services are designed to uncover and articulate your unique identity, ensuring that every touchpoint resonates with your target audience. From initial brand strategy and research to crafting a compelling visual identity, we work closely with you to build a brand that is authentic, memorable, and impactful.",
             brandImg: brand
-        },
+        }, 
         {
             image: image3,
             title: "Afaqs! startup brands ",
