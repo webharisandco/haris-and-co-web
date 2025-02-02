de11788286b6a9a10f902eec0ca587511cb7c997
Afsal Ka <afsalafzz63@gmail.com>
2025-02-02 21:20:18 +0530

    branding section done

--- src/pages/branding/index.jsx
+++ src/pages/branding/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import BrandImages from "./BrandImages";
 
 function Branding() {
   return (
-    <div className="bg-black">
+    <div className="bg-[#0E0E0E]">
       <Banner />
+      <BrandImages/>
     </div>
   );
 }
