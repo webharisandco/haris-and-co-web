c30d080f04e9a1151faa6645ad34a187c7bfd84f
Afsal Ka <afsalafzz63@gmail.com>
2025-02-03 10:20:25 +0530

    branding page responsive done

--- src/pages/branding/Banner.jsx
+++ src/pages/branding/Banner.jsx
@@ -6,19 +6,23 @@ function Banner() {
     <div className=" px-[16px] lg:px-[80px] pt-[200px] ">
       <div className=" flex flex-col gap-[35px]">
         <p className="text-white/50 text-[20px] font-[thin]">Branding </p>
-        <p className="text-[55px] 2xl:text-[64px] text-white font-[helvetica-medium]">
+        <p className="text-[32px] lg:text-[55px] 2xl:text-[64px] text-white font-[helvetica-medium]">
           Partnering with Toalla De tela
         </p>
-        <p className="text-white/50 text-[38px] font-[thin] ">
+        <p className="text-white/50 text-[24px] lg:text-[38px] font-[thin] ">
           Branding & Identity, Websites <br /> & Digital Platforms
         </p>
       </div>
       <div className="w-full flex justify-center mt-[100px]">
-        <img className="w-fit h-fit" src={Branding1} alt="" />
+        <img
+          className="w-full lg:w-fit h-[252px] object-cover lg:h-fit"
+          src={Branding1}
+          alt=""
+        />
       </div>
-      <div className="flex justify-between w-full items-center ">
-        <div className="w-8/12 my-[70px] pt-[80px] pr-[80px] border-r border-white">
-          <p className="text-[26px] lg:text-[26px] 2xl:text-[32px] text-white font-[thin]">
+      <div className="flex flex-col lg:flex-row lg:justify-between w-full lg:items-center ">
+        <div className="lg:w-8/12 pb-[38px] lg:pb-0 lg:my-[70px] pt-[38px] lg:pt-[80px] lg:pr-[80px] border-b lg:border-b-0 lg:border-r border-white">
+          <p className="text-[16px] lg:text-[26px] 2xl:text-[32px] text-white font-[thin]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -31,7 +35,7 @@ function Banner() {
           </p>
         </div>
 
-        <div className="w-3/12">
+        <div className="lg:w-3/12 my-[40px] lg:my-0">
           <ul className="text-white flex flex-col gap-[20px] text-[22px]">
             <li>Visual Identity Design</li>
             <li>Brand Messaging and Voice</li>
