11513accc8a3cca672e383d6e5ec8817cb96066e
undefined-coder-1 <devs4webs@gmail.com>
2025-02-10 18:21:58 +0530

    rebased

--- src/pages/awards/AwardSection.jsx
+++ src/pages/awards/AwardSection.jsx
@@ -8,7 +8,7 @@ export default function AwardSection({ image, title, date, desc, brandImg }) {
             <div className="font-[thin] flex flex-col gap-10 justify-between md:px-10">
                 <div className='text-base md:text-[20px] text-black/75 flex justify-between lg:justify-start '><span>{title} </span> <span className='hidden md:inline'>.</span> <span>{date}</span></div>
                 <p className='text-[20px] md:text-[18px]  leading-tight -mt-4'>{desc}</p>
-                <img src={brandImg} alt="" className='w-[150px]' />
+                <img src={brandImg} alt="" className='w-[150px]' /> 
             </div>
         </div>
     )
