2c80490e0a365a45648cbe2b29c5cbf10693b6f6
undefined-coder-1 <devs4webs@gmail.com>
2025-02-13 17:15:24 +0530

    error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// import { useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './routes/Router';
 import { useLocation } from 'react-router-dom';
