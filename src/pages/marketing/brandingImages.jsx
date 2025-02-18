5ef7202d0da08ea270f632fe3d12143e6e4da431
undefined-coder-1 <devs4webs@gmail.com>
2025-02-18 11:00:01 +0530

    client page change

--- src/pages/marketing/brandingImages.jsx
+++ src/pages/marketing/brandingImages.jsx
@@ -13,10 +13,10 @@ import Br9 from "../../assets/images/marketing/br9.svg";
 function BrandingImages() {
   return (
     <div className="bg-white px-[13px] ">
-      <div className="hidden lg:flex p-[80px]">
+      <div className="hidden lg:flex p-[80px] justify-center">
         <img src={img5} alt="" />
       </div>
-      <div className="flex lg:hidden py-[38px] items-center justify-center  flex-col gap-[30px] w-full">
+      <div className="flex lg:hidden py-[38px]   flex-col gap-[30px] w-full">
         <img src={Br1} alt="Brand 1" />
         <img src={Br2} alt="Brand 2" />
         <img src={Br3} alt="Brand 3" />
