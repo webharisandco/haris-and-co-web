498654d179220dabd08c3a459d032de9cc6ed514
undefined-coder-1 <devs4webs@gmail.com>
2025-02-19 11:45:33 +0530

    vercel deploy add new folder

---
+++ src/pages/clients/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./banner";
+import BrandingImages from "./brandingImages";
+
+function Clients() {
+  return (
+    <div className="">
+      <Banner />
+      <BrandingImages />
+    </div>
+  );
+}
+
+export default Clients;
