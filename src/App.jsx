a566e4116c9c93da46b2d8381da57a73aee71bb9
Afsal Ka <afsalafzz63@gmail.com>
2025-05-28 13:52:57 +0530

    udpated scroll to top function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useEffect } from 'react';
 import './App.css';
 import AppRouter from './routes/Router';
-import { useLocation } from 'react-router-dom';
+import ScrollToTop from './components/layout/ScrollTop';
 import('./Font.css');
 import('./Font/helvetica-neue-55.css')
 
 function App() {
-  // const { pathname } = useLocation(); 
 
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, [pathname]); 
 
   return (
     <div className="App">
+      <ScrollToTop/>
       <AppRouter />
     </div>
   );
