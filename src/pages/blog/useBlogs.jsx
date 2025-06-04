79dd8355dff7c5b29991140e06ae6b0ef5ee4c26
Afsal Ka <afsalafzz63@gmail.com>
2025-06-04 15:40:04 +0530

    update blog

--- src/pages/blog/useBlogs.jsx
+++ src/pages/blog/useBlogs.jsx
@@ -20,19 +20,19 @@ export default function useBlogs() {
     {
       img: blogImg1,
       type: "Web Development & UI/UX",
-      title: "Styling an online presence for Disha Creationz!",
+      title: "What is SEO in Digital Marketing? How It Works and Why It’s Important",
       date: "November 07, 2024 ",
     },
     {
       img: blogImg2,
       type: "Web Development & UI/UX",
-      title: "Styling an online presence for Disha Creationz!",
+      title: "SEO Content Writing: What It Is, How It Works & Why It Matters",
       date: "May 24, 2025 ",
     },
     {
       img: blogImg3,
       type: "Web Development & UI/UX",
-      title: "Styling an online presence for Disha Creationz!",
+      title: "How Digital Marketing is Transforming Real Estate",
       date: "May 24, 2025 ",
     },
   ];
