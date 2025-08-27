f8a85bad1c1ca8ddec74004b21c53b8d3f96ce68
Afsal Ka <afsalafzz63@gmail.com>
2025-08-27 17:49:40 +0530

    career page update

--- src/pages/careers/index.jsx
+++ src/pages/careers/index.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import Banner from './Banner'
-import JobCard from './JobCard'
-import WhyWork from './WhyWork'
+import React from "react";
+import Banner from "./Banner";
+import JobCard from "./JobCard";
+import WhyWork from "./WhyWork";
 
 export default function Careers() {
-    return (
-        <div>
-            <Banner />
-            <WhyWork />
-            <div className="bg-black  px-[16px] n md:px-[80px] py-[55px] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-3 xl:gap-6">
-                <JobCard jobName={"Graphic Designer"} />
-                <JobCard jobName={"Graphic Designer"} />
-                <JobCard jobName={"Graphic Designer"} className="hidden lg:flex"/>
-                <JobCard jobName={"Graphic Designer"} className="hidden lg:flex"/>
-                <JobCard jobName={"Graphic Designer"}  className="hidden lg:flex"/>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Banner />
+      <WhyWork />
+      <div className="bg-black  px-[16px] n md:px-[80px] py-[55px] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-3 xl:gap-6">
+        <JobCard jobName={"Graphic Designer"} />
+        <JobCard jobName={"Video Editor"} />
+        <JobCard jobName={"Cinematographer"} />
+        <JobCard jobName={"Cinematographer"} />
+        <JobCard jobName={"Performance Marketing Executive"} />
+        <JobCard jobName={"Project Coordinator"} />
+        <JobCard jobName={"Project Coordinator"} />
+        <JobCard jobName={"SEO Content Writer"} />
+        <JobCard jobName={"SEO Content Writer"} />
+        <JobCard jobName={"SEO Content Writer"} />
+        <JobCard jobName={"Social Media Content Writer"} />
+        <JobCard jobName={"Social Media Content Writer"} />
+        <JobCard jobName={"Sales Trainer"} />
+        <JobCard jobName={"Appointment Setter"} />
+        <JobCard jobName={"Business Development Representatives"} />
+        <JobCard jobName={"Web Developer"} />
+        <JobCard jobName={"HR Executive"} />
+        <JobCard jobName={"Marketing Head"} />
+        <JobCard jobName={"Receptionist"} />
+        <JobCard jobName={"Creative Director"} />
+      </div>
+    </div>
+  );
 }
